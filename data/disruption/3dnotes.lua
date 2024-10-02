50f65f89aa9820a9e46f3775d2417acb62bc92b8
xav07theboi <xavtheman07@gmail.com>
2024-10-02 14:33:59 -0600

    furiosity fixes

--- data/disruption/3dnotes.lua
+++ data/disruption/3dnotes.lua
@@ -1,6 +1,5 @@
 function onSpawnNote(membersIndex, noteData, noteType, isSustainNote, strumTime)
     yeah = getRandomBool(100*(1-(getHealth()/2)))
-    spawned = spawned + 1
     if yeah == true then
         setPropertyFromGroup('unspawnNotes', 0, 'texture', 'noteskins/NOTE_assets-3D-GA')
     end
